9fa2fb73b87c2b050fd5fc76624fb25fbe021789
Cheyenne Schabracq <cschabra@f0r3s13.codam.nl>
2024-04-25 17:13:40 +0200

    Add setup parse test

---
+++ tests/parser_test.cpp
@@ -0,0 +1,14 @@
+#include "tests.h"
+
+void	parse_test()
+{
+	t_data	test;
+	std::cout << RESET << "Parse test:\n";
+	std::cout << ;
+	std::cout << std::endl;
+}
+
+int	main(void)
+{
+	parse_test();
+}
