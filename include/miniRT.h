3c9e571dd1934a03b33a8b1b78f4f41b13c262a8
Cheyenne Schabracq <cschabra@f0r3s15.codam.nl>
2024-05-28 13:55:27 +0200

    Add normal distribution for random numbers

--- include/miniRT.h
+++ include/miniRT.h
@@ -6,7 +6,7 @@
 /*   By: cschabra <cschabra@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/05 17:47:28 by cschabra      #+#    #+#                 */
-/*   Updated: 2024/05/27 17:18:22 by vvan-der      ########   odam.nl         */
+/*   Updated: 2024/05/28 13:49:26 by cschabra      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@
 # define FOREVER 1
 # define OFFSET 0.00001f
 # define THRESHHOLD 0.1
-# define NUM_RAYS 10
+# define NUM_RAYS 1000
 # define MAX_BOUNCES 50
 
 // void		test_multiple_renders(t_data *data, uint32_t x, uint32_t y);
